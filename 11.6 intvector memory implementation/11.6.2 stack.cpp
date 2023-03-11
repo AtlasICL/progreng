f114d8fe1bbb08af81b7ae58589f0104828aeeb1
Atlas Emre Acarsoy <emre.acarsoy22@imperial.ac.uk>
2023-03-11 18:33:32 +0000

    updated 11.6.2

---
+++ 11.6 intvector memory implementation/11.6.2 stack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+typedef int stack_t;
+
+struct StackLayer{
+    stack_t val;
+    StackLayer* next;
+};
+
+class Stack{
+
+public:
+
+    // TODO:
+    // implement methods:
+    // pop
+    // push
+    // top
+
+    int size() const{
+        return size_s;
+    }
+
+private:
+
+    StackLayer* top_layer;
+    int size_s;
+
+};
+
+// void test_stack(const Stack& s){
+//     s.top();
+//     s.size();
+// }
